4780fd2f5dcc46f93667959038c7ca6747dfbf29
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2025-01-30 23:39:53 +0530

    Build Example 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
 
 void main() {
   runApp(const MainApp());
+}
+
+const names = ['John', 'Jane', 'Jack', 'Jill'];
+
+extension RandomeElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(names.getRandomElement());
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +24,60 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
       ),
+      body: StreamBuilder(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final button = ElevatedButton(
+              onPressed: cubit.pickRandomName,
+              child: const Text('Pick a random name'),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    Text(snapshot.data.toString()),
+                    button,
+                  ],
+                );
+            }
+          }),
     );
   }
 }
