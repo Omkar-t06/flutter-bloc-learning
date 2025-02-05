c90c577fe77d45b8e212ac8a2d986b3d1244ec53
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2025-02-05 23:43:16 +0530

    Refactor logic for Testing

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,14 +1,25 @@
-import 'package:bloc_tutorial/main.dart' show PersonUrl;
+// import 'package:bloc_tutorial/bloc/bloc_action.dart' show PersonUrl;
+import 'dart:developer' as devtools show log;
 
-extension UrlString on PersonUrl {
-  String get urlString {
-    switch (this) {
-      case PersonUrl.persons1:
-        return 'http://10.0.2.2:5500/api/person1.json';
-      case PersonUrl.persons2:
-        return 'http://10.0.2.2:5500/api/person2.json';
-    }
-  }
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+// extension UrlString on PersonUrl {
+//   String get urlString {
+//     switch (this) {
+//       case PersonUrl.persons1:
+//         return 'http://10.0.2.2:5500/api/person1.json';
+//       case PersonUrl.persons2:
+//         return 'http://10.0.2.2:5500/api/person2.json';
+//     }
+//   }
+// }
+
+extension IsEqualToIgnoringOrdering<T> on Iterable<T> {
+  bool isEqualToIgnoringOrdering(Iterable<T> other) =>
+      length == other.length &&
+      {...this}.intersection({...other}).length == length;
 }
 
 extension Subscript<T> on Iterable<T> {
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:bloc_tutorial/bloc/bloc_action.dart';
+import 'package:bloc_tutorial/bloc/person.dart';
+import 'package:bloc_tutorial/bloc/persons_bloc.dart';
 import 'package:bloc_tutorial/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:developer' as devtools show log;
-
-extension Log on Object {
-  void log() => devtools.log(toString());
-}
 
 void main() {
   runApp(const MainApp());
@@ -28,86 +26,12 @@ class MainApp extends StatelessWidget {
   }
 }
 
-@immutable
-abstract class LoadAction {
-  const LoadAction();
-}
-
-@immutable
-class LoadPersonsAction implements LoadAction {
-  final PersonUrl url;
-  const LoadPersonsAction({required this.url}) : super();
-}
-
-enum PersonUrl {
-  persons1,
-  persons2,
-}
-
-@immutable
-class Person {
-  final String name;
-  final int age;
-  const Person({
-    required this.name,
-    required this.age,
-  });
-  Person.fromJson(Map<String, dynamic> json)
-      : name = json['name'] as String,
-        age = json['age'] as int;
-
-  @override
-  String toString() => 'Person (name = $name, age = $age)';
-}
-
 Future<Iterable<Person>> getPersons(String url) => HttpClient()
     .getUrl(Uri.parse(url))
     .then((req) => req.close())
     .then((resp) => resp.transform(utf8.decoder).join())
     .then((str) => json.decode(str) as List<dynamic>)
     .then((list) => list.map((e) => Person.fromJson(e)));
-
-@immutable
-class FetchResult {
-  final Iterable<Person> persons;
-  final bool isRetrievedFromCache;
-  const FetchResult({
-    required this.persons,
-    required this.isRetrievedFromCache,
-  });
-
-  @override
-  String toString() =>
-      'FetchResult (isRetrievedFromCache = $isRetrievedFromCache, persons = $persons)';
-}
-
-class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
-  final Map<PersonUrl, Iterable<Person>> _cache = {};
-  PersonsBloc() : super(null) {
-    on<LoadPersonsAction>(
-      (event, emit) async {
-        final url = event.url;
-        if (_cache.containsKey(url)) {
-          // we have the value in the cache
-          final cachedPersons = _cache[url]!;
-          final result = FetchResult(
-            persons: cachedPersons,
-            isRetrievedFromCache: true,
-          );
-          emit(result);
-        } else {
-          final persons = await getPersons(url.urlString);
-          _cache[url] = persons;
-          final result = FetchResult(
-            persons: persons,
-            isRetrievedFromCache: false,
-          );
-          emit(result);
-        }
-      },
-    );
-  }
-}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -126,7 +50,8 @@ class HomePage extends StatelessWidget {
                 onPressed: () {
                   context.read<PersonsBloc>().add(
                         const LoadPersonsAction(
-                          url: PersonUrl.persons1,
+                          url: persons1Url,
+                          loader: getPersons,
                         ),
                       );
                 },
@@ -137,8 +62,9 @@ class HomePage extends StatelessWidget {
               TextButton(
                 onPressed: () {
                   context.read<PersonsBloc>().add(
-                        const LoadPersonsAction(
-                          url: PersonUrl.persons2,
+                        LoadPersonsAction(
+                          url: persons2Url,
+                          loader: getPersons,
                         ),
                       );
                 },
